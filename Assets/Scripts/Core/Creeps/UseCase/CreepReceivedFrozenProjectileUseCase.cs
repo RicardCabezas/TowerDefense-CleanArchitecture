17e03f3bb630c14a5261f8e2241f70016549e719
Ricard Cabezas <r.cabezas.diez@gmail.com>
2022-11-24 20:03:28 +0100

    Files cleanuo

--- Assets/Scripts/Core/Creeps/UseCase/CreepReceivedFrozenProjectileUseCase.cs
+++ Assets/Scripts/Core/Creeps/UseCase/CreepReceivedFrozenProjectileUseCase.cs
@@ -1,32 +1,28 @@
 using Core.Turrets.Configs;
 using Core.Turrets.Entities;
 using Events;
-using UnityEngine;
 
 namespace Core.Creeps.UseCase
 {
-    public class CreepReceivedRegularProjectileUseCase : ICreepReceivedProjectile
+    public class CreepReceivedFrozenProjectileUseCase : ICreepReceivedProjectile
     {
         private readonly CreepRepository _creepRepository;
         private readonly ProjectilesRepository _turretsRepository;
-        private readonly DestroyCreepUseCase _destroyCreepUseCase;
 
-        public CreepReceivedRegularProjectileUseCase()
+        public CreepReceivedFrozenProjectileUseCase()
         {
             _turretsRepository = ServiceLocator.Instance.GetService<ProjectilesRepository>();
             _creepRepository = ServiceLocator.Instance.GetService<CreepRepository>();
-            _destroyCreepUseCase = new DestroyCreepUseCase();
         }
 
         public void Execute(int creepInstanceId, int projectileInstanceId)
         {
-            var projectileConfig = _turretsRepository.GetProjectileConfig<RegularProjectile>(projectileInstanceId);
+            var projectileConfig = _turretsRepository.GetProjectileConfig<FrozenProjectile>(projectileInstanceId);
             var creep = _creepRepository.GetCreepEntity(creepInstanceId);
-            creep.Health -= projectileConfig.Damage;
+            creep.CurrentSpeed -= projectileConfig.SpeedDebuff;
 
-            if (creep.Health <= 0)
-                _destroyCreepUseCase.Destroy(creepInstanceId);
-
+            if (creep.CurrentSpeed < 0)
+                creep.CurrentSpeed = 0;
         }
     }
 }
